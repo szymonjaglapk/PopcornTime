c545e3e8232ad8c0c7820274d462cc0b84321d1c
Szymon <szymon.jagla@student.pk.edu.pl>
2025-04-14 22:27:05 +0200

    add JWT authentication

---
+++ backend/src/main/java/com/popcorntime/service/AuthenticationService.java
@@ -0,0 +1,61 @@
+package com.popcorntime.service;
+
+import com.popcorntime.dto.AuthenticationRequest;
+import com.popcorntime.dto.AuthenticationResponse;
+import com.popcorntime.dto.RegisterRequest;
+import com.popcorntime.model.Type;
+import com.popcorntime.model.TypeEnum;
+import com.popcorntime.model.User;
+import com.popcorntime.repository.TypeRepository;
+import com.popcorntime.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final TypeRepository typeRepository;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .name(request.getName())
+                .surname(request.getSurname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .types(new HashSet<>())
+                .build();
+        Type type = typeRepository.findByName(TypeEnum.TYPE_USER.name())
+                .orElse(Type.builder().name(TypeEnum.TYPE_USER.name()).build());
+        user.addType(type);
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
